9fb0edd0ee76dae5021ff8d0cfbb4f6683f4e739
Markus Graeser <markus.graeser+gh@gmail.com>
2019-03-15 14:50:19 +0100

    Added code for second button which toggles colour inversion; minor cleanup

--- magni.py
+++ magni.py
@@ -16,9 +16,11 @@ SCALE_FACTORS = [DEFAULT_FACTOR, 5, 10]
 factor = SCALE_FACTORS[0]  # use first entry as initial factor on boot up
 
 
-PIN_NUMBER = 10   # physical pin number of GPIO used for button
-BOUNCE_TIME = 300 # ms to wait till a new button event is generated (avoid double click artefacts)
-DELAY = 0.01      # s to sleep between polling the keyboard
+PIN_NUMBER_SCALE = 10 # physical pin number of GPIO for scale button
+PIN_NUMBER_COLOR =  8 # physical pin number of GPIO for colour mode
+
+BOUNCE_TIME_MS = 300   # ms to wait till a new button event is generated (avoid double click artefacts)
+DELAY_S = 0.01        # s to sleep between polling the keyboard
 
 # default view of raspivid without specific scaling
 # raspivid -f -t 0 -rot 180
@@ -40,6 +42,15 @@ def getch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
 
+# toggle -ifx parameter between normal colours and inverted colours
+def invert(channel):
+    global factor
+    global RASPIVID
+    if '-ifx' in RASPIVID:
+        i = RASPIVID.index('-ifx')
+        RASPIVID[i + 1] = 'negative' if RASPIVID[i + 1] == 'none' else 'none'
+        # reload with toggled image effect
+        scale(factor)
 
 # convert a scale factor to the values needed by raspivid's roi parameter
 def scale2roi(scale_factor):
@@ -101,9 +112,17 @@ def scale(new_factor):
 def initPushbutton():    
     GPIO.setwarnings(False) # Ignore warning for now
     GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-    GPIO.setup(PIN_NUMBER, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-
-    GPIO.add_event_detect(PIN_NUMBER, GPIO.RISING, callback=nextFactor, bouncetime=BOUNCE_TIME) # Setup event on pin 10 rising edge, ignore additional signals in under BOUNCE_TIME ms
+    
+    # Set pin for scale button to be an input pin and set initial value to be pulled low (off)
+    GPIO.setup(PIN_NUMBER_SCALE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    # Setup event on rising edge, ignore additional signals in under BOUNCE_TIME_MS ms
+    GPIO.add_event_detect(PIN_NUMBER_SCALE, GPIO.RISING, callback=nextFactor, bouncetime=BOUNCE_TIME_MS) 
+    
+    
+    # Set pin for colour invert button to be an input pin and set initial value to be pulled low (off)
+    GPIO.setup(PIN_NUMBER_INVERT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    # Setup event on rising edge, ignore additional signals in under BOUNCE_TIME_MS ms
+    GPIO.add_event_detect(PIN_NUMBER_INVERT, GPIO.RISING, callback=invert, bouncetime=BOUNCE_TIME_MS)
 
 
 
@@ -137,17 +156,11 @@ while ord(char) != ESCAPE_KEY:
         nextFactor('')
         
     # pressing '/' toggles colour inversion
-    elif char == '/' and '-ifx' in RASPIVID:
-        i = RASPIVID.index('-ifx')
-        if RASPIVID[i + 1] == 'none':
-            RASPIVID[i + 1] = 'negative'
-        else:
-            RASPIVID[i + 1] = 'none'
-        # reload with toggled image effect
-        scale(factor)
+    elif char == '/':
+        invert(0)
         
     # wait a bit to not block the processor
-    time.sleep(DELAY)
+    time.sleep(DELAY_S)
 
 
 GPIO.cleanup() # Clean up
